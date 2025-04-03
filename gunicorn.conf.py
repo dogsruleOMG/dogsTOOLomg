84e0c722508366c3eb834b56c31d1ec69132d856
knels <dogsruleomg@gmail.com>
2025-04-03 18:59:23 -0400

    Update Gunicorn configuration and fix application path

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,9 @@
+import os
+
+bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
 workers = 4
-bind = "0.0.0.0:10000"
-timeout = 120 
+timeout = 120
+worker_class = "sync"
+accesslog = "-"
+errorlog = "-"
+chdir = "." 
